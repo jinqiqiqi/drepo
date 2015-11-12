af97734c8a1b27967e02da725d1d3d9ea2bdc43a
Eric.King <qi.jin@happysports.com>
2015-11-12 16:27:21 +0800

    add nginx 1.6.3

--- livestream/shells/build.sh
+++ livestream/shells/build.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-cd /opt/src/nginx-1.8.0
+cd /opt/src/nginx-1.6.3
 
-./configure --add-module=/opt/src/nginx-rtmp-module --prefix=/opt/nginx180/
+./configure --add-module=/opt/src/nginx-rtmp-module --add-module=/opt/src/nginx-rtmp-module/hls --prefix=/opt/nginx163/
 make
 make install
 
-cd /opt/src/
+# cd /opt/src/
 
-git clone https://github.com/jp9000/obs-studio.git
-cd obs-studio
+# git clone https://github.com/jp9000/obs-studio.git
+# cd obs-studio
 
-mkdir build && cd build
-cmake ..
-make -j4
-sudo make install 
+# mkdir build && cd build
+# cmake ..
+# make -j4
+# sudo make install 
